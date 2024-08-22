cbddb5b733a4f144f820c2308347afd8cdb0f8b2
khushi <khushihp7903@gmail.com>
2024-08-22 13:09:30 +0530

    changed-condition-allow-only-processed-events-for-auth

--- event-based/src/main/java/com/eventbasedarchitechture/event_based/interceptor/AuthorizeValidator.java
+++ event-based/src/main/java/com/eventbasedarchitechture/event_based/interceptor/AuthorizeValidator.java
@@ -1,23 +1,35 @@
 package com.eventbasedarchitechture.event_based.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+
+@Component
 public class AuthorizeValidator implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthorizeValidator.class);
 
     @Override
     public boolean preHandle(
             HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception{
 
         String status = request.getParameter("status");
+        logger.info("Status received: {}", status);
 
-        if(status!=null && status.equalsIgnoreCase("PENDING")){
-            return true;
-        }else{
-            response.setStatus(403);
-            return false;
+        // Check if status is "PROCESSED"
+        if (status != null && status.equalsIgnoreCase("PROCESSED")) {
+            logger.info("Status is PROCESSED, proceeding with request");
+            return true;  // Continue processing the request
+        } else {
+            logger.warn("Status is NOT PROCESSED, blocking request");
+            response.setStatus(403);  // Forbidden
+            return false;  // Reject the request
         }
     }
 }
--- event-based/src/main/java/com/eventbasedarchitechture/event_based/interceptor/Register.java
+++ event-based/src/main/java/com/eventbasedarchitechture/event_based/interceptor/Register.java
@@ -1,11 +1,11 @@
 package com.eventbasedarchitechture.event_based.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class Register implements WebMvcConfigurer {
 
     @Autowired
